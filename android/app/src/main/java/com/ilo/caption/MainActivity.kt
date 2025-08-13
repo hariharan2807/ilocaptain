d3842984cb2db0de1066ec3b8977edbf0055fa55
hari <hariharan12.gtech@gmail.com>
2025-08-13 15:32:46 +0530

    Renamed

--- android/app/src/main/java/com/ilo/caption/MainActivity.kt
+++ android/app/src/main/java/com/ilo/caption/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.ilocaptain
+package com.ilo.caption
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
@@ -11,7 +11,8 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "iLoCaptain"
+  override fun getMainComponentName(): String = "ILO Captain
+"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
--- android/app/src/main/java/com/ilo/caption/MainApplication.kt
+++ android/app/src/main/java/com/ilo/caption/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.ilocaptain
+package com.ilo.caption
 
 import android.app.Application
 import com.facebook.react.PackageList
